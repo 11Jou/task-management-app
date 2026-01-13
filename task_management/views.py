5ba1ea4cfe16b5f2d1c25e5c7c2bfb69133fac6c
Youssef Mohamed <youssefmuhamed634@gmail.com>
2026-01-13 13:37:31 +0200

    updates

--- task_management/views.py
+++ task_management/views.py
@@ -26,9 +26,6 @@ class ListCreateTaskView(ListCreateAPIView):
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-        
-    def perform_update(self, serializer):
-        serializer.save(user=self.request.user)
 
 
 class RetrieveUpdateDestroyTaskView(RetrieveUpdateDestroyAPIView):
